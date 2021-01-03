830c7b97548381a05dd17596b3003a246207f84d
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-01-03 14:15:58 +0900

    Zミノが画面上部から落ちてくるように

---
+++ TetrisCreated/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TetrisCreated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.drawTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 315);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // drawTimer
+            // 
+            this.drawTimer.Enabled = true;
+            this.drawTimer.Interval = 10;
+            this.drawTimer.Tick += new System.EventHandler(this.drawTimer_Tick);
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Enabled = true;
+            this.UpdateTimer.Interval = 1000;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 339);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer drawTimer;
+        private System.Windows.Forms.Timer UpdateTimer;
+    }
+}
+
---
+++ TetrisCreated/Form1.cs
@@ -0,0 +1,90 @@
+﻿// Sakamaki Daiki
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TetrisCreated
+{
+    public partial class Form1 : Form
+    {
+
+
+        // ブロックリスト作成
+        List<Rectangle> rectList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 各ロード関数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // rectをnewしてその中に呼出し
+            rectList = new List<Rectangle>
+            {
+                // ブロック座標情報リストに追加
+                new Rectangle(0, 0, 10, 10),
+                new Rectangle(10, 0, 10, 10),
+                new Rectangle(10, 10, 10, 10),
+                new Rectangle(20, 10, 10, 10)
+            };
+        }
+
+        /// <summary>
+        /// PictureBox描画関数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // foreach を使い描画
+            foreach (Rectangle rect in rectList)
+            {
+                e.Graphics.FillRectangle(Brushes.Red, rect);
+            }
+        }
+
+        /// <summary>
+        /// 時間経過関数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UpdateTimer_Tick_1(object sender, EventArgs e)
+        {
+            // for文でListのCountをループさせる
+            for (int i = 0; i < rectList.Count; i++)
+            {
+                // rectListの i 配列分を Rectangle 型の _r に代入を行う
+                Rectangle _r = rectList[i];
+                // rectListの中身を引き継いだ _r 変数を使って時間経過とともに座標を動かす
+                _r = new Rectangle(_r.Left, _r.Top + 10, _r.Width, _r.Height);
+                // 最終的にlistに代入
+                rectList[i] = _r;
+            }
+        }
+
+        /// <summary>
+        /// 時間経過描画初期化処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void drawTimer_Tick(object sender, EventArgs e)
+        {
+            // Refreshをかけ更新処理
+            pictureBox1.Refresh();
+        }
+
+    }
+}
